451279a20796d8335b2b47ca0128bb49ce6fbbf6
Neeraj Gautam <gautamneeraj492@gmail.com>
2025-03-24 17:03:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import streamlit as st
 import pickle
 
-st.subheader('Taxi Uber Ride Prediction')
-ppw=st.number_input('Enter 	Priceperweek')
-pop=st.number_input('Enter Population')
-mntin=st.number_input('Enter Monthlyincome')
-appm=st.number_input('Enter Averageparkingpermonth')
-button=st.button('Predict!')
-
-
+st.subheader('Iris Classification App')
+sl=st.number_input('Enter sepal-length')
+sw=st.number_input('Enter sepal-width')
+pl=st.number_input(' Enter petal-length')
+pw=st.number_input('Enter petal-width')
+button=st.button('Predict')
 if button:
-    model=pickle.load(open('taxi.pkl','rb'))
-   
-    res=model.predict([[ppw,pop,mntin,appm]])[0]
-    st.markdown(f'''
-   Number Of Weakly Riders:{res}
-                ''')
+   kn=pickle.load(open('iris.pkl','rb'))
+   res=kn.predict([[sl,sw,pl,pw]])[0]
+   st.markdown(f' The Species of the Flower is : {res}')
